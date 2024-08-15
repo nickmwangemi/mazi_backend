fb005f08bb4612e40fd952ca3fd2aab32b777bb2
nickmwangemi <nickmwangemi@gmail.com>
2024-08-15 15:23:45 +0300

    register models in admin panel

--- station_management/admin.py
+++ station_management/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import SwappingStation, Battery, IoTData
 
-# Register your models here.
+admin.site.register(SwappingStation)
+admin.site.register(Battery)
+admin.site.register(IoTData)
